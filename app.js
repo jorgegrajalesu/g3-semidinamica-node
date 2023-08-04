4da9ff64e44ed76d57651ff1d585baeeb4e8a76a
jorgegrajalesu <jegosystem@gmail.com>
2023-08-03 20:24:55 -0500

    030823

--- app.js
+++ app.js
@@ -2,5 +2,13 @@
 para crear un webserver en un entorno local
 */
 const express = require('express');
+// el webserver se le asigne al archivo app.js
+const app = express();
+const port = 9595;
+
+// mostrar contenido de la carpeta public
+app.use(express.static('public'));
+
+
 
 
